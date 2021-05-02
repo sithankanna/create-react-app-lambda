937fc7b13b0f6183d7092da13d951d7a7408f56e
sithankanna <sithankanna@gmail.com>
2021-05-02 16:18:34 +0100

    Update logic directory

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import React from "react"
 import "./App.css"
-import getNumber from "./Logic"
+import getNumber from "./lambda/Logic"
 
 class App extends React.Component {
   constructor(props) {
